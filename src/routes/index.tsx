c905a5089cee45a7dd2e389793501a9d27f76b50
Paisen <pvnt20@gmail.com>
2026-04-18 19:33:54 +0530

    refactor: remove legacy SolidJS application files and configurations
    - Deleted favicon.ico, app.css, app.tsx, entry-client.tsx, entry-server.tsx, global.d.ts, and various route files.
    - Removed tsconfig.json and vite.config.ts to clean up project structure.
    - Added a new index.html file with a loading message styled using Tailwind CSS.

--- src/routes/index.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main class="flex items-center justify-center h-screen text-center mx-auto p-4">
-      <h1 class="max-6-xs text-6xl uppercase my-16 font-bold text-[#fb8500]">
-        じゅんびちゅう...
-      </h1>
-    </main>
-  );
-}
